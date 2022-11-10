1070b329cc7918c875322e2c79a576b9d4494aca
Vlad Bazhenov <Bazhenov393@yandex.ru>
2022-11-10 22:07:19 +0300

    second attempt

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,37 @@
-const MODAL_ACTIVE_CLASS = 'modal__active';
+const modalActiveClass = 'modal_active';
 const openModalButton = document.querySelector('.profile__editButton');
 const modal = document.querySelector('.modal');
 const modalContent = modal.querySelector('.modal__content');
 const modalCloseButton = modal.querySelector('.modal__close');
-const submitInput = modal.querySelector('.modal__save');
+const submitInput = modal.querySelector('.modal__input_type_save');
 
 openModalButton.addEventListener('click', () => {
-  modal.classList.add(MODAL_ACTIVE_CLASS);
+  modal.classList.add(modalActiveClass);
 });
 
 modal.addEventListener('click', (event) => {
   if (!modalContent.contains(event.target) 
    || event.target === modalCloseButton 
    || event.target === submitInput) {
-    modal.classList.remove(MODAL_ACTIVE_CLASS);
+    modal.classList.remove(modalActiveClass);
   }
 });
 
 /*card-grid__likeButton*/
-const ADD_LIKE_BUTTON_ACTIVE = 'сard-grid__likeButton_active';
+const addLikeButtonActive = 'сard-grid__likeButton_active';
 const likeButtonActive = document.querySelectorAll('.card-grid__likeButton');
 const arrButtonLike = Array.from(likeButtonActive);
 
 arrButtonLike.forEach((item) => {
   item.addEventListener('click', () => {
-    item.classList.toggle(ADD_LIKE_BUTTON_ACTIVE);
+    item.classList.toggle(addLikeButtonActive);
   });
 });
 
 /*form+modal*/
 const mainForm = modal.querySelector('.modal__form');
-const nameInput = mainForm.querySelector('.modal__name');
-const professionInput = mainForm.querySelector('.modal__profession');
+const nameInput = mainForm.querySelector('.modal__input_type_name');
+const professionInput = mainForm.querySelector('.modal__input_type_profession');
 const profileContainer = document.querySelector('.profile__info');
 const nameText = profileContainer.querySelector('.profile__name');
 const professionText = profileContainer.querySelector('.profile__profession');
@@ -42,3 +42,8 @@ mainForm.addEventListener('submit', (event) => {
   professionText.textContent = professionInput.value;
 });
 
+/*name+profession*/
+openModalButton.addEventListener('click', (event) => {
+  nameInput.value = nameText.textContent;
+  professionInput.value = professionText.textContent;
+});
