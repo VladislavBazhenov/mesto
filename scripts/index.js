832eb5dd015aff6a6d71b53000b0418401024ef1
Vlad Bazhenov <Bazhenov393@yandex.ru>
2022-11-22 21:50:20 +0300

    before valid yandex

--- scripts/index.js
+++ scripts/index.js
@@ -1,108 +1,90 @@
 //Добавление класса в попапы 
 const modalActiveClass = 'modal_active';
 
+//Общие функции открытия/закрытия попапов
+function openPopup(popup) {
+  popup.classList.add(modalActiveClass);
+}
+
+function closePopup(popup) {
+  popup.classList.remove(modalActiveClass);
+}
+
+function closeModalProfile(event) {
+  if (!modalEditContent.contains(event.target) 
+  || event.target === buttonCloseEditModal 
+  || event.target === submitEditInput) {
+   closePopup(modalEdit);
+ }
+}
+
+function closeModalAdd(event) {
+  if (!modalAddContent.contains(event.target) 
+   || event.target === buttonCloseAddModal 
+   || event.target === submitAddInput) {
+    closePopup(modalAdd);
+  }
+}
+
 /*Попап редактирования профиля*/
-const openModalEditButton = document.querySelector('.profile__editButton');
+const buttonOpenEditModal = document.querySelector('.profile__editButton');
 const modalEdit = document.querySelector('.modal-edit');
 const modalEditContent = modalEdit.querySelector('.modal__content');
-const modalEditCloseButton = modalEdit.querySelector('.modal__close');
+const buttonCloseEditModal = modalEdit.querySelector('.modal__close');
 const submitEditInput = modalEdit.querySelector('.modal__input_type_save');
 
-openModalEditButton.addEventListener('click', () => {
-  modalEdit.classList.add(modalActiveClass);
+buttonOpenEditModal.addEventListener('click', () => {
+  openPopup(modalEdit);
 });
 
-modalEdit.addEventListener('click', (event) => {
-  if (!modalEditContent.contains(event.target) 
-   || event.target === modalEditCloseButton 
-   || event.target === submitEditInput) {
-    modalEdit.classList.remove(modalActiveClass);
-  }
-});
+modalEdit.addEventListener('click', closeModalProfile);
 
 /*Попап добавления карточек*/
-const openModalAddButton = document.querySelector('.profile__addButton');
+const buttonOpenAddModal = document.querySelector('.profile__addButton');
 const modalAdd = document.querySelector('.modal-add');
 const modalAddContent = modalAdd.querySelector('.modal__content')
-const modalAddCloseButton = modalAdd.querySelector('.modal__close');
+const buttonCloseAddModal = modalAdd.querySelector('.modal__close');
 const submitAddInput = modalAdd.querySelector('.modal__input_type_save');
 
-openModalAddButton.addEventListener('click', () => {
-  modalAdd.classList.add(modalActiveClass);
+buttonOpenAddModal.addEventListener('click', () => {
+  openPopup(modalAdd);
 });
 
-modalAdd.addEventListener('click', (event) => {
-  if (!modalAddContent.contains(event.target) 
-   || event.target === modalAddCloseButton 
-   || event.target === submitAddInput) {
-    modalAdd.classList.remove(modalActiveClass);
-  }
-});
-
+modalAdd.addEventListener('click', closeModalAdd);
 
 /*form+modalEdit*/
-const editForm = modalEdit.querySelector('.modal__form');
-const nameInput = editForm.querySelector('.modal__input_type_name');
-const professionInput = editForm.querySelector('.modal__input_type_profession');
+const profileForm = modalEdit.querySelector('.modal__form');
+const nameInput = profileForm.querySelector('.modal__input_type_name');
+const professionInput = profileForm.querySelector('.modal__input_type_profession');
 const profileContainer = document.querySelector('.profile__info');
 const nameText = profileContainer.querySelector('.profile__name');
 const professionText = profileContainer.querySelector('.profile__profession');
 
-editForm.addEventListener('submit', (event) => {
+profileForm.addEventListener('submit', (event) => {
   event.preventDefault();
   nameText.textContent = nameInput.value;
   professionText.textContent = professionInput.value;
 });
 
-
-
 /*name+profession*/
-openModalEditButton.addEventListener('click', (event) => {
+buttonOpenEditModal.addEventListener('click', (event) => {
   nameInput.value = nameText.textContent;
   professionInput.value = professionText.textContent;
 });
 
-
-
 //Карточки
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
-
-const cardContainer = document.querySelector('.card-grid');
-const addForm = modalAdd.querySelector('.modal__form');
+const cardsContainer = document.querySelector('.card-grid');
+const cardForm = modalAdd.querySelector('.modal__form');
 const modalPicture = document.querySelector('.modal-picture');
-const openPicture = modalPicture.querySelector('.modal-picture__image');
+const modalPictureContent = modalPicture.querySelector('.modal__content');
+const cardModalPicture = modalPicture.querySelector('.modal-picture__image');
+const modalPictureCloseButton = modalPicture.querySelector('.modal__close');
 const titlePicture = modalPicture.querySelector('.modal__title_picture');
-const placeNameInput = addForm.querySelector('.modal__input_type_place-name');
-const imageLinkInput = addForm.querySelector('.modal__input_type_image-link');
+const placeNameInput = cardForm.querySelector('.modal__input_type_place-name');
+const imageLinkInput = cardForm.querySelector('.modal__input_type_image-link');
 const cardSample = document.querySelector('#template-cards').content.querySelector('.card-grid__element');
 
-addForm.addEventListener('submit', (event) => {
+cardForm.addEventListener('submit', (event) => {
   event.preventDefault();
   renderCard({ 
     name: placeNameInput.value ,
@@ -116,39 +98,43 @@ const createCard = (card) => {
   const copyCardSample = cardSample.cloneNode(true);
   const picture = copyCardSample.querySelector('.card-grid__item');
   const title = copyCardSample.querySelector('.card-grid__title');
-  const urnButton = copyCardSample.querySelector('.urn');
+  const deleteButton = copyCardSample.querySelector('.urn');
   const likeButton = copyCardSample.querySelector('.card-grid__likeButton');
+  
   title.textContent = card.name;
-  picture.setAttribute('src', card.link);
-  picture.setAttribute('alt', card.name);
-  urnButton.addEventListener('click', (event) =>{
-    event.target.closest('.card-grid__element').remove();
+  picture.src = card.link;
+  picture.alt = card.name;
+  deleteButton.addEventListener('click', () =>{
+    copyCardSample.remove();
   });
 
+
   picture.addEventListener('click', () => {
-    modalPicture.classList.add(modalActiveClass);
-    openPicture.setAttribute('src', card.link);
+    openPopup(modalPicture);
+    cardModalPicture.src = card.link;
+    cardModalPicture.alt = card.name;
     titlePicture.textContent = card.name;
   });
-  modalPicture.addEventListener('click', (event) => {
-    if (!modalAddContent.contains(event.target) 
-    || event.target === modalAddCloseButton) {
-    modalPicture.classList.remove(modalActiveClass);
-    }
-  });
-    likeButton.addEventListener('click', () => {
-      likeButton.classList.toggle(addLikeButtonActive);
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle(buttonLikeActiveClass);
   });
 
   return copyCardSample;
 };
 
+modalPicture.addEventListener('click', (event) => {
+  if (!modalPictureContent.contains(event.target) 
+  || event.target === modalPictureCloseButton) {
+  closePopup(modalPicture);
+  }
+});
+
 const renderCard = (card) => {
-  cardContainer.prepend(createCard(card));
+  cardsContainer.prepend(createCard(card));
 };
 
 initialCards.forEach((card) => {
   renderCard(card);
 });
 
-const addLikeButtonActive = 'сard-grid__likeButton_active';
+const buttonLikeActiveClass = 'сard-grid__likeButton_active';
